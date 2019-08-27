7cf9cf81eb9087173f44ba5c6b2862487985fcb3
yangming <88888888>
2019-08-27 14:48:43 +0800

    base java8

--- base/src/main/java/com.yamhto.base/JoinTest.java
+++ base/src/main/java/com.yamhto.base/JoinTest.java
@@ -1,22 +1,28 @@
 package com.yamhto.base;
 
+import java.util.concurrent.CountDownLatch;
+
 public class JoinTest {
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
         Thread t1 = new Thread(new Runnable() {
             public void run() {
-                for (int i = 0; i < 8; i++) {
+                for (int i = 0; i < 80; i++) {
 
                     System.out.println(Thread.currentThread().getName() + ":" + i);
                 }
+                //countDownLatch.countDown();
             }
         }, "t1");
         t1.start();
         t1.join();
+        //countDownLatch.await();
 
         Thread t2 = new Thread(new Runnable() {
             public void run() {
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 100; i++) {
 
                     System.out.println(Thread.currentThread().getName() + ":" + i);
                 }
@@ -27,7 +33,7 @@ public class JoinTest {
 
         new Thread(new Runnable() {
             public void run() {
-                for (int i = 0; i < 9; i++) {
+                for (int i = 0; i < 90; i++) {
 
                     System.out.println(Thread.currentThread().getName() + ":" + i);
                 }
