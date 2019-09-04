accdafe5b3d5545a676a3b3ccd45e27bfaec2464
yangming <88888888>
2019-09-04 15:07:10 +0800

    java 8 Function test

--- java-base/jdk-8/src/main/java/com/yamhto/jdk/Calc.java
+++ java-base/jdk-8/src/main/java/com/yamhto/jdk/Calc.java
@@ -1,7 +1,9 @@
 package com.yamhto.jdk;
 
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author yamhto
@@ -15,12 +17,17 @@ public class Calc {
 
     /**
      * 抽象出行为，也就是将方法当作参数传递
+     *
      * @param in
      * @param fun
      * @return
      */
-    public String compuse(Integer in, Function<Integer,String> fun){
+    public String compuse(Integer in, Function<Integer, String> fun) {
         return fun.apply(in);
+    }
+
+    public Integer calc(Integer in1, Integer in2, BiFunction<Integer, Integer, Integer> fun) {
+        return fun.apply(in1, in2);
     }
 
 
@@ -29,6 +36,8 @@ public class Calc {
         Calc calc = new Calc();
 
         System.out.println(calc.compuse(2, (i) -> String.valueOf(i * i)));
+
+        System.out.println(calc.calc(9, 2, (i1, i2) -> i1 * i2));
     }
 
 }
---
+++ java-base/jdk-8/src/main/java/java 8.md
@@ -0,0 +1,12 @@
+### Function vs Consume vs Supplier vs Predicate
+```text
+ Function Vs BiFunction:
+ Function one input --> one Result
+ BiFunction two input --> one Result
+
+ Predicate Vs BiPredicate AND
+ Consume Vs BiConsume is same as Function Vs BiFunction:
+
+ Supplier:
+ none input --> one Result
+```
